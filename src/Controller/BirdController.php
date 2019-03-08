d030b11be886f499fa99fdf82fab488f2a702b78
Anthony Gorski <tony.gorski13@gmail.com>
2019-03-08 14:26:49 +0100

    Exo fini

--- src/Controller/BirdController.php
+++ src/Controller/BirdController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use App\Model\BirdData;
 /**
  * Class BirdController
@@ -15,11 +16,15 @@ class BirdController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(SessionInterface $session)
     {
         $bird = new BirdData;
+
+        $lastBird = $session->get('last_bird', 'None');
+
         return $this->render('default/bird.html.twig', [
             'names' => $bird->getBird(),
+            'lastBird' => $lastBird,
         ]);
     }
 
@@ -27,14 +32,16 @@ class BirdController extends AbstractController
      *
      * @Route("/bird/{id}", name="birdDetails", requirements={"id"="\d+"})
      */
-    public function bird($id)
+    public function bird($id, SessionInterface $session)
     {
         $bird = new BirdData;
         $bird = $bird->getBirdId($id);
 
         if($bird === false) {
-            throw $this->createNotFoundException("Pas d'oiseau");
+            throw $this->createNotFoundException("Pas doiseau");
         }
+
+        $session->set('last_bird', $bird['name']);
 
         return $this->render('default/birdDetail.html.twig', [
             'name' => $bird['name'],
@@ -49,5 +56,16 @@ class BirdController extends AbstractController
     public function pdf()
     {
         return $this->file('pdf/angry_birds_2015_calendar.pdf');
+    }
+
+
+    /**
+     * @Route("/api/birds", name="bird-json")
+     */
+    public function api()
+    {
+        $bird = new BirdData;
+
+        return $this->json($bird->getBird());
     }
 }
